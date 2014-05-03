54616b9fc5a9bddeb3b1743e4cca0c59e200e1d1
tzm <tzm@d-rekords.de>
2014-05-03 12:59:35 +0200

    1. Test mit kaputt

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,6 +1,6 @@
 # Edit this for your own project dependencies
-# (todo)
-OPAM_DEPENDS="ocamlfind ounit re"
+# (blubber)
+OPAM_DEPENDS="ocamlfind kaputt"
 	 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
