57ef46aa27a99b145e7321363df2515ca17a8199
tzm <tzm@d-rekords.de>
2014-05-04 15:53:43 +0200

    write and read zeichen

--- firstTest.ml
+++ firstTest.ml
@@ -2,10 +2,14 @@ open Kaputt.Abbreviations
 open Test
 ;;
 
-Test.add_simple_test ~title:"testing works..." (fun () ->
-  Assert.equal_int 2 2)
+Test.add_simple_test ~title:"write and read a zeichen ..." (fun () ->
+  let plan = Begriff.empty in
+  let zeichen = Begriff.zeichen_of_string "test" in
+  let _ = Begriff.add_zeichen zeichen plan in
+  let found = Begriff.find_zeichen (Begriff.atom_of_int 1) plan in
+  Assert.is_some found;
+  Assert.equal (Some (Begriff.zeichen_of_string "test")) found); 
 ;;
 
 launch_tests ()
 ;;
-
